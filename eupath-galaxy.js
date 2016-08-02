b4eeb51fda0767247f9f034c6d320c5b298f1145
Cris Lawrence <crisl@upenn.edu>
2016-08-02 20:03:09 +0000

    css and js files to control galaxy welcome page L&F from Eupath.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathGalaxy@75741 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ eupath-galaxy.js
@@ -0,0 +1,16 @@
+document.getElementById("eupath-intro").innerHTML = "" +
+		"The come has come, the walrus said, to speak of many things." +
+		" Of ships and sails and sealing wax.  Of cabbages and kings.";
+
+document.getElementById("eupath-links").innerHTML = "" +
+        "<li><a href='#'>Link A</a></li>" +
+		"<li><a href='#'>Link B</a></li>" +
+		"<li><a href='#'>Link C</a></li>";
+
+document.getElementById("eupath-workflows").innerHTML = "" +
+		"<tr><td><h3>Workflow A</h3></td><td><a href='#'>Describe</a></td><td><a href='#'>Import</a></td></tr>" +
+        "<tr><td colspan='3'>Beware the Jabberwock my son, with claws that catch and teeth that bite</td></tr>";
+  
+  
+
+
