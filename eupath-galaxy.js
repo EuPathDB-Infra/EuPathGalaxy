31fe38fc0f1bb765c615b5ddeb0a1b4bdb9afbf1
Cristina Aurrecoechea <aurreco@uga.edu>
2017-06-19 12:21:04 +0000

    typo
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathGalaxy/branches/build29@82330 735e2a04-f8fc-0310-8a1b-f2942603c481

--- eupath-galaxy.js
+++ eupath-galaxy.js
@@ -37,7 +37,7 @@ document.getElementById("eupath-workflows").innerHTML = "" +
         "Profile a transcriptome and analyze differential gene expression.<br>Tools: FastQC, Trimmomatic, TopHat2, CuffLinks, CuffDiff.<br></p>" +
         "<p title=\"Click to run the workflow with your datasets\"><a id='workflow5' href='javascript:void(0)' onClick='import_and_run_workflow(\"59725c868c65b63f\")'>EuPathDB Workflow for Variant Calling, single-read sequencing</a><br>" +
         "Profile and analyse SNPs.<br>Tools: Bowtie2, FreeBayes, and SnpEff<br></p>" +
-        "<p title=\"Click to run the workflow with your datasets\"><a id='workflow6' href='javascript:void(0)' onClick='import_and_run_workflow(\"78f0ccbcef5e3211\")'>EuPathDB Workflow for Variant Calling, paired-ed sequencing</a><br>" +
+        "<p title=\"Click to run the workflow with your datasets\"><a id='workflow6' href='javascript:void(0)' onClick='import_and_run_workflow(\"78f0ccbcef5e3211\")'>EuPathDB Workflow for Variant Calling, paired-end sequencing</a><br>" +
         "Profile and analyse SNPs.<br>Tools: Bowtie2, FreeBayes, and SnpEff<br></p>";
 /**
  * A series of ajax calls to import a published workflow (if not already imported) and get its id,
