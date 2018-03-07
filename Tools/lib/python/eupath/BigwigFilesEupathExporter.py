a78d165566c3ec1726cb3ff6550c2bd81c3551cf
Cris Lawrence <crisl@upenn.edu>
2018-03-07 17:33:06 +0000

    Fixed bugs
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathGalaxy/trunk@87622 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Tools/lib/python/eupath/BigwigFilesEupathExporter.py
+++ Tools/lib/python/eupath/BigwigFilesEupathExporter.py
@@ -6,30 +6,31 @@ import ntpath
 class BigwigFilesExport(EupathExporter.Export):
 
     # Constants
-    TYPE = "Bigwig Tracks"
+    TYPE = "BigwigFiles"
     VERSION = "1.0"
 
     def __init__(self, args):
         EupathExporter.Export.__init__(self,
-                                       TYPE,
-                                       VERSION,
+                                       BigwigFilesExport.TYPE,
+                                       BigwigFilesExport.VERSION,
                                        None,
                                        args)
 
-        # beyond the standard 5 params, this exporter requires one or more pairs of args: dataset1 dataset1.refGenome dataset2...
+        # beyond the standard 5 params, this exporter requires one or more pairs of args: dataset1 dataset1.refGenome
+        # dataset2...
         if len(args) < 7:
             raise EupathExporter.ValidationException("The tool was passed an insufficient numbers of arguments.")
 
         # grab first provided ref genome, as the master
         self._refGenome = ReferenceGenome.Genome(args[6])
-        self._datasetInfos = [];
+        self._datasetInfos = []
         
         # process variable number of [dataset refgenome] pairs.
         # confirm that all regenomes are identical.
-        for i in args[5::2]:   # start on 6th arg, increment by 2
-            if args[i+1] != self._refGenome:
+        for i in range(5, len(args), 2):   # start on 6th arg, increment by 2
+            if args[i+1] != self._refGenome.identifier:
                 raise EupathExporter.ValidationException("All provided bigwig datasets must have the same reference genome.")
-            self._datasetInfos.append({ntpath.basename(args[i]), args[i]})
+            self._datasetInfos.append({"name": ntpath.basename(args[i]), "path":args[i]})
 
 
     def identify_dependencies(self):
@@ -49,4 +50,4 @@ class BigwigFilesExport(EupathExporter.Export):
         """
         :return: A list containing the dataset files accompanied by their EuPathDB designation.
         """
-        return self.datasetInfos
+        return self._datasetInfos
