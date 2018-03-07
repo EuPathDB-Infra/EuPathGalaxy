d66b57fd0612c5c2e25553dca0c8bcbee1485de9
Cris Lawrence <crisl@upenn.edu>
2018-03-07 21:49:47 +0000

    Added a 3rd file component holding the dataset name
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathGalaxy/trunk@87641 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Tools/lib/python/eupath/BigwigFilesEupathExporter.py
+++ Tools/lib/python/eupath/BigwigFilesEupathExporter.py
@@ -18,19 +18,19 @@ class BigwigFilesExport(EupathExporter.Export):
 
         # beyond the standard 5 params, this exporter requires one or more pairs of args: dataset1 dataset1.refGenome
         # dataset2...
-        if len(args) < 7:
+        if len(args) < 8:
             raise EupathExporter.ValidationException("The tool was passed an insufficient numbers of arguments.")
 
         # grab first provided ref genome, as the master
-        self._refGenome = ReferenceGenome.Genome(args[6])
+        self._refGenome = ReferenceGenome.Genome(args[7])
         self._datasetInfos = []
         
         # process variable number of [dataset refgenome] pairs.
         # confirm that all regenomes are identical.
-        for i in range(5, len(args), 2):   # start on 6th arg, increment by 2
-            if args[i+1] != self._refGenome.identifier:
+        for i in range(5, len(args), 3):   # start on 6th arg, increment by 2
+            if args[i+2] != self._refGenome.identifier:
                 raise EupathExporter.ValidationException("All provided bigwig datasets must have the same reference genome.")
-            self._datasetInfos.append({"name": ntpath.basename(args[i]), "path":args[i]})
+            self._datasetInfos.append({"name": args[i+1], "path":args[i]})
 
 
     def identify_dependencies(self):
