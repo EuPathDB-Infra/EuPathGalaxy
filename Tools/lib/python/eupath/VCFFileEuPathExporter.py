c6998c440606eb1581711a627f34aa5192d1865d
Ross Madden <ross.madden@glasgow.ac.uk>
2019-08-23 11:19:17 +0000

    VCF validator - working as of new. Needing FreeBayes update in worlkflow on Galaxy - issue with header that causes vcf val to (correctly) fail.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathGalaxy/trunk@100478 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Tools/lib/python/eupath/VCFFileEuPathExporter.py
@@ -0,0 +1,57 @@
+import EupathExporter
+import ReferenceGenome
+import sys
+import os
+import subprocess
+
+
+class VCFFileExport(EupathExporter.Export):
+
+    # Constants
+    TYPE = "VCFFile"
+    VERSION = "1.0"
+
+    def __init__(self, args):
+        EupathExporter.Export.__init__(self,
+                                       VCFFileExport.TYPE,
+                                       VCFFileExport.VERSION,
+                                       'validateVCF',
+                                       args)
+
+        self._datasetInfos = []
+
+        # open manifest file
+        manifestPath = "/tmp/manifest." + str(os.getpid()) + ".txt"
+        manifest = open(manifestPath, "w+")
+
+        for i in range(6, len(args), 2):
+            print >> sys.stdout, i, args[i]
+            samplename = args[i+1]
+            #filename = samplename + "." + args[i+2]
+        ## Note in the xml this will need the right variables passed in - e.g. sample name, file format.
+        
+
+            self._datasetInfos.append({"name": samplename, "path": args[i]})
+            print >> manifest, samplename # + "\t" + filename 
+                
+
+        self._datasetInfos.append({"name": "manifest.txt", "path": manifestPath})
+        
+        # Need this? 
+        # self._refGenome = ReferenceGenome.Genome(args[10])
+
+
+        # if len(args) < 10:
+        #     raise EupathExporter.ValidationException("The tool was passed too few arguments.")
+
+        
+    def identify_dataset_files(self):
+        """
+        :return: A list containing the dataset files accompanied by their EuPathDB designation.
+        """
+        return self._datasetInfos
+
+
+    def identify_projects(self):
+
+        return [self._refGenome.project]
